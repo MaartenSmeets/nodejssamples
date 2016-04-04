75bc7be13efd926b723a63814c27a72defa9fc29
MaartenSmeets <a7HUQbz6mDzfsGHA3peC>
2016-04-04 15:44:39 +0200

    Adding tests

--- nodesimpleorchestration/cache.js
+++ nodesimpleorchestration/cache.js
@@ -4,17 +4,20 @@ var express = require('express')
     , url = require('url');
 
 var router = express.Router();
-
 var cache = {};
 
+function logger(msg) {
+    //console.log.apply(this,arguments);
+}
+
 router.use(function timeLogStart(req, res, next) {
-    console.log('Request start');
+    logger('Request start');
     res.locals.startTimeHR = process.hrtime();
     next();
 });
 
 router.put('/', function(req, res, next) { //process 
-    console.log("PUT: "+req.body.CACHE_KEY+": "+req.body.CACHE_VALUE);
+    logger("PUT: "+req.body.CACHE_KEY+": "+req.body.CACHE_VALUE);
     var result_msg = {};
     if (req.body.CACHE_KEY in cache) {
         var result_msg = { "result": "Cache entry updated: " + req.body.CACHE_KEY + " = " + req.body.CACHE_VALUE };
@@ -33,7 +36,7 @@ router.put('/', function(req, res, next) { //process
 });
 
 router.get('/:CACHE_KEY', function(req, res, next) {
-    console.log("GET: "+req.params['CACHE_KEY']);
+    logger("GET: "+req.params['CACHE_KEY']);
     if (req.params['CACHE_KEY'] in cache) {
         var result_msg = { "result": cache[req.params['CACHE_KEY']] };
         res.setHeader('Content-Type', 'application/json');
@@ -48,7 +51,7 @@ router.get('/:CACHE_KEY', function(req, res, next) {
 });
 
 router.delete('/:CACHE_KEY', function(req, res, next) {
-    console.log("DELETE: "+req.params['CACHE_KEY']);
+    logger("DELETE: "+req.params['CACHE_KEY']);
     if (req.params['CACHE_KEY'] in cache) {
         delete cache[req.params['CACHE_KEY']];
         res.setHeader('Content-Type', 'application/json');
@@ -66,7 +69,7 @@ router.delete('/:CACHE_KEY', function(req, res, next) {
 
 router.use(function timeLogEnd(req, res, next) {
     var durationHR = process.hrtime(res.locals.startTimeHR);
-    console.log('Request end. Duration: %ds %dms',durationHR[0],durationHR[1]/1000000);
+    logger("Request end. Duration: %ds %dms",durationHR[0],durationHR[1]/1000000);
     next();
 });
 

2e4dc4ccac3478e3c506a60496d7b678412c31fd
Maarten Smeets <maarten.smeets@amis.nl>
2016-08-11 07:28:32 +0200

    Adding error handling

--- nodexml/helloworldservice.js
+++ nodexml/helloworldservice.js
@@ -15,7 +15,6 @@ var helloworldservice = {};
 var servicewsdl = 'helloworld.wsdl';
 
 router.use(bodyParser.text({ type: '*/*' }));
-
 router.use(function timeLogStart(req, res, next) {
     myutils.logger('Request start');
     res.locals.startTimeHR = process.hrtime();
@@ -58,23 +57,22 @@ router.post('/', function (req, res, next) { //process
         function (result, cb) {
             myutils.logger('Processing JSONized XML message');
             //myutils.logger('Input: ' + JSON.stringify(result));
-            try {
-                var body = result["Envelope"]["Body"];
-                //finding the correct elements
-                var sayHello = myutils.search("sayHello", body);
-                var firstName = myutils.search("firstName", sayHello);
-                var firstNameValue = myutils.search("_", firstName);
-                cb(null, 'Hello ' + firstNameValue);
-            } catch (err) {
-                cb(err, null);
-            }
+            var body = result["Envelope"]["Body"];
+            //finding the correct elements
+            var sayHello = myutils.search("sayHello", body);
+            var firstName = myutils.search("firstName", sayHello);
+            var firstNameValue = myutils.search("_", firstName);
+            cb(null, 'Hello ' + firstNameValue);
         }
     ],
         function (err, results) {
+            if (err) {
+                results = "I don't know your name";
+            }
             //building the response
             var builder = new Builder();
             //var xmlresponse = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:examples:helloservice"><soapenv:Header/><soapenv:Body><urn:sayHelloResponse soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><greeting xsi:type="xsd:string">?</greeting></urn:sayHelloResponse></soapenv:Body></soapenv:Envelope>'
-            
+
             var jsonresponse = {
                 "soapenv:Envelope": {
                     "$": {
@@ -109,7 +107,7 @@ router.post('/', function (req, res, next) { //process
 
             res.setHeader('Content-Type', 'application/xml');
             res.statusCode = 200;
-            res.end(xml);
+            res.end(xmlresponse);
             next(err, results);
         }
     );

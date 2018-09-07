107f02e717633340f2c86a82757fd017feb0b968
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2018-09-07 09:05:51 +0200

    Adding quick SOAP service

---
+++ fastsoaphelloworld/app.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var fs = require('fs')
+var morgan = require('morgan')
+var path = require('path')
+var app = express()
+
+var expresssoap = require('express-soap');
+var soap = expresssoap.soap;
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+    flags: 'a'
+})
+
+var xml = require('fs').readFileSync('helloworldservice.wsdl', 'utf8');
+
+// setup the logger
+app.use(morgan('tiny', {
+    stream: accessLogStream
+}))
+
+//Hosting the service
+app.use('/soap/helloworld', soap({
+    services: { /* Services */
+        Hello_Service: { /* Ports */
+            Hello_Port: { /* Methods */
+                sayHello: function(args, callback, headers, req) {
+                    return {
+                        greeting: 'Hello ' + args.firstName.$value
+                    };
+                }
+            }
+        }
+    },
+    xml: xml
+}));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
